362600b88befd0a4758c3bab6f143b8b8587f7c5
Numan-Bello <numanbashir81997@gmail.com>
2025-01-16 11:00:43 +0100

    second commit

---
+++ src/com/neotech/lesson02/JavaProgrammer.java
@@ -0,0 +1,19 @@
+package com.neotech.lesson02;
+
+public class JavaProgrammer 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("I am a Java Programmer!");
+		System.out.println("I study at Neotech!");
+		System.out.println("I Love Java!");
+		
+		System.out.println("------------------------");
+		
+		// 2nd way
+		
+		System.out.println("I am a Java Programmer!\nI study at Neotech!\nI Love Java!");
+		
+	}
+}
---
+++ src/com/neotech/lesson02/Variables.java
@@ -0,0 +1,17 @@
+package com.neotech.lesson02;
+
+public class Variables 
+{
+	public static void main(String[] args) 
+	{
+		
+		int someNumber = 15;
+		
+		System.out.println(15);
+		System.out.println("15");
+		
+		System.out.println(someNumber);
+		System.out.println(someNumber);
+		
+	}
+}
